e87a63cb64e80a6d03ce7de0ca1fdcd5d2131946
ecarhuapoma <81769655+ecarhuapoma@users.noreply.github.com>
2022-02-01 00:12:20 -0500

    custom burger!
    it looks ok

--- dev/js/burgerAnimation.js
+++ dev/js/burgerAnimation.js
@@ -3,19 +3,26 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 gsap.registerPlugin(DrawSVGPlugin);
 
 gsap.set(".burger-lines",{transformOrigin:"center"});
-gsap.set("#outline",{drawSVG: "0%"});
+gsap.set(".burger-lines:nth-child(2)",{drawSVG:"0% 22.5%"});
+gsap.set(".burger-lines:nth-child(3)",{drawSVG:"78% 100%"});
+// gsap.set("#outline",{drawSVG: "0%"});
 
 const topTL = new gsap.timeline();
-topTL.to(".burger-lines:nth-child(1)",{duration:0.25, y:"+=8", fill:"#FFF"})
-.to(".burger-lines:nth-child(1)",{duration:0.25, rotation:45});
+topTL.to(".burger-lines:nth-child(1)",{duration:0.25})
+.to(".burger-lines:nth-child(1)",{duration:0.25, rotation:-45});
 
 const bottomTL = new gsap.timeline();
-bottomTL.to(".burger-lines:nth-child(3)",{duration:0.25, y:"-=8", fill:"#FFF"})
-.to(".burger-lines:nth-child(3)",{duration:0.25, rotation:-45});
+bottomTL.to(".burger-lines:nth-child(2)",{duration:0.25})
+.to(".burger-lines:nth-child(2)",{drawSVG:"37% 62%",duration:0.25})
+.to(".burger-lines:nth-child(3)",{drawSVG:"50% 50%",duration:0.25},"-=.25");
+// const middleTL = new gsap.timeline();
+// bottomTL.to(".burger-lines:nth-child(3)",{duration:0.25})
+// .to(".burger-lines:nth-child(3)",{drawSVG:"50% 50%",duration:0.25});
 
-const middleTL = new gsap.timeline();
-middleTL.to(".burger-lines:nth-child(2)",{duration:0.25, scale:0, fill:"#FFF"})
-.to("#outline",{duration:0.25, drawSVG:"100%", stroke:"#FFF"});
+
+// const middleTL = new gsap.timeline();
+// middleTL.to(".burger-lines:nth-child(2)",{duration:0.25, scale:0, fill:"#FFF"})
+// .to("#outline",{duration:0.25, drawSVG:"100%", stroke:"#FFF"});
 
 
 export const burgerTL = new gsap.timeline({paused:true});
@@ -23,6 +30,6 @@ export const burgerTL = new gsap.timeline({paused:true});
 
 burgerTL.add(topTL,"burger")
     .add(bottomTL,"burger")
-    .add(middleTL,"burger")
+    // .add(middleTL,"burger")
 
 
